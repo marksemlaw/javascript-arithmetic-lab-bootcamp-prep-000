839a5a142713ed77c33e275f80d66608fa8fc981
Mark Semotiuk <marksem@gmail.com>
2018-07-17 16:26:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 function add(a, b) {
-  a + b
+  return a + b
 }
 
 function subtract(a, b) {
-  a - b
+  return a - b
 }
 
 function multiply(a, b) {
-  a * b
+  return  a * b
 }
 
 function divide(a, b) {
-  a / b
+  return a / b
 }
 
 function inc(a) {
-  ++a
+  return ++a
 }
 
 function dec(a) {
-  --a
+  return --a
 }
 
 function makeInt(a) {
-  parseInt(a, 10)
+  return parseInt(a, 10)
   }
 
 function preserveDecimal(a) {
-  parseFloat(a)
+  return parseFloat(a)
   }
